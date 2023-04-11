2e721560b0a6c0404a02e567b1debe72bd263ca1
danielaltamiranoborja <64813513+danielaltamirano1993@users.noreply.github.com>
2023-04-10 20:16:18 -0500

    Update main.js

--- script/main.js
+++ script/main.js
@@ -5,3 +5,20 @@
     __hasProp = {}.hasOwnProperty,
     __extends = function(child, parent) { for (var key in parent) { if (__hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor(); child.__super__ = parent.prototype; return child; };
 
+  utils = {
+    now: function() {
+      return new Date().getTime();
+    },
+    snap: function(value, snapSize) {
+      return Math.floor(value / snapSize) * snapSize;
+    },
+    rand: function(min, max) {
+      var range;
+      if (!(max != null)) {
+        max = min;
+        min = 0;
+      }
+      range = max - min;
+      return Math.floor((Math.random() * range) + min);
+    }
+  };
