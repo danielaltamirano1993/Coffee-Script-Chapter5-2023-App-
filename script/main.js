6d986549590b312f7986f2a138d944f0bff2fad0
danielaltamiranoborja <64813513+danielaltamirano1993@users.noreply.github.com>
2023-04-13 10:33:30 -0500

    Update main.js

--- script/main.js
+++ script/main.js
@@ -561,3 +561,22 @@
 
     return Ninja;
 
+  })(Entity);
+
+  Block = (function() {
+
+    Block.prototype.touchable = false;
+
+    Block.prototype.solid = false;
+
+    Block.prototype.climbable = false;
+
+    function Block() {}
+
+    Block.prototype.update = function() {};
+
+    Block.prototype.render = function(gfx, x, y) {};
+
+    return Block;
+
+  })();
